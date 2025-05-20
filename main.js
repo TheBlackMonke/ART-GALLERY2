5c25bee74c07f2060bc59c3e6a3a74c1148268df
TheBlackMonke <zedstylish2@gmail.com>
2025-05-19 22:11:59 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.gallery-item img').forEach(image => {
+    image.addEventListener('click', () => {
+        const src = image.src;
+        const lightbox = document.createElement('div');
+        lightbox.classList.add('lightbox');
+        lightbox.innerHTML = `<img src="${src}" alt="Lightbox Image"><span class="close">&times;</span>`;
+        document.body.appendChild(lightbox);
+
+        lightbox.querySelector('.close').addEventListener('click', () => {
+            document.body.removeChild(lightbox);
+        });
+    });
+});
